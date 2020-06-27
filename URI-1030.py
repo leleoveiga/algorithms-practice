363eed218a71fb5ddb444a2db6e71468e34f17b8
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-06-27 05:02:19 -0300

    .

---
+++ URI-1030.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+for i in range(n):
+    x = list(map(int, input().split()))
+    lista = []
+    for j in range(x[0]):
+        lista.append(j+1)
+    eliminados = 0
+    j = x[1]-1
+    while eliminados < x[0]-1:
+        print(j)
+        print(lista)
+        
+        del lista[j]
+        eliminados += 1
+        passadas = 0
+        j += x[1]-1
+        while j > len(lista)-1:
+            j -= len(lista)
+    print("Case %d: %d" % (i+1, lista[0]))
