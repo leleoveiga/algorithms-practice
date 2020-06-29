a64d190880f23c8036a16913510e54f9e1303009
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2020-06-28 23:40:20 -0300

    .

--- URI-1030.py
+++ URI-1030.py
@@ -5,16 +5,8 @@ for i in range(n):
     lista = []
     for j in range(x[0]):
         lista.append(j+1)
-    eliminados = 0
-    j = x[1]-1
-    while eliminados < x[0]-1:
-        #print(j)
-        #print(lista)
-        
+    j = 0
+    while len(lista) > 1:
+        j = (j+x[1]-1) % len(lista)
         del lista[j]
-        eliminados += 1
-        passadas = 0
-        j += x[1]-1
-        if j > len(lista)-1:
-            j = j % len(lista)
     print("Case %d: %d" % (i+1, lista[0]))
